5a6c5d1435798e832ae1824d31b931d954dac41e
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-11-28 14:10:34 +0100

    news on homepage are now browsable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Test } from '../Test';
 import { News } from '../News';
+import { TestService } from '../test.service';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +13,45 @@ export class HomeComponent implements OnInit {
 
   popularTests:Test[];
   news:News[];
-  constructor() { }
+  currentPage:number =0;
+  testNumber:number=0;
+  totalPages:number;
+  constructor(private newsService: NewsService,private testService:TestService) { }
 
   ngOnInit() {
+    this.newsService.getAllNews(this.currentPage,5).subscribe(news => {
+      this.news = news.content;
+      this.totalPages = news.totalPages;
+    });
+    this.testService.getAllTest(0,5,"desc","rating",true,"",0,5,0,9999).subscribe(stuff =>this.popularTests = stuff.content);
+  }
+
+  nextTest() {
+    if(this.testNumber < this.popularTests.length-1) {
+      this.testNumber++;
+    }
+    else {
+      this.testNumber =0;
+    }
+  }
+
+  previousTest() {
+    if(this.testNumber > 0) {
+      this.testNumber++;
+    }
+    else {
+      this.testNumber =this.popularTests.length-1;
+    }
+  }
+
+  getOlderNews() {
+    this.currentPage++;
+    this.newsService.getAllNews(this.currentPage,5).subscribe(news => this.news = news.content);
+  }
+
+  getNewNews() {
+    this.currentPage--;
+    this.newsService.getAllNews(this.currentPage,5).subscribe(news => this.news = news.content);
   }
 
 }
