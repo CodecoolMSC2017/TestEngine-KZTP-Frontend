6fb8bb0c241a33cc3a5528108f4350feff65fade
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-11-29 10:59:11 +0100

    added UsersTest model,further design changes

---
+++ src/app/UsersTest.ts
@@ -0,0 +1,11 @@
+import { Test } from "./Test";
+import { User } from "./user";
+
+export class UsersTest {
+  id:number;
+  user:User;
+  test:Test;
+  maxPoints:number;
+  actualPoints:number;
+  percentage:number;
+}
